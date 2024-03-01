d3ffdad21dd80437cd1181914912af59f844a163
Satya Adhiyaksa Ardy <satyaadhiyaksa.2020@student.uny.ac.id>
2024-03-01 23:22:13 +0700

    update

--- index.js
+++ index.js
@@ -38,11 +38,16 @@ whatsappClient.on("message_create", async (msg) => {
 whatsappClient.on("message_revoke_everyone", async (after, before) => {
   if (after.from === "status@broadcast") return;
 
-  // Notify the user that a message was deleted from where it was sent and send the original message to the admin. get from contact name
-  const contact = await whatsappClient.getContactById(after.from);
-  const sender = contact.pushname ?? after.from;
-  const message = `Pesan dari ${sender} dihapus: ${before.body}`;
-  whatsappClient.sendMessage(MY_NUMBER, message);
+  // Notify the user that a message was deleted from where it was sent and send the original message to the admin. get from contact name or if group, get from group name
+  const contact = await whatsappClient.getContactById(before.from);
+  const sender = contact.pushname ?? before.from;
+  const message = before.body;
+  const chatId = before.chatId._serialized;
+  const chat = await whatsappClient.getChatById(chatId);
+  const chatName = chat.isGroup ? chat.name : sender;
+
+  const originalMessage = `Pesan dari ${chatName} dihapus: ${message} (dari ${sender})`;
+  whatsappClient.sendMessage(MY_NUMBER, originalMessage);
 });
 
 whatsappClient.initialize();
